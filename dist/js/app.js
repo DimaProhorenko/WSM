733859dea3299669b935365974ba4800cb30c041
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-10-02 23:41:32 +0300

    ADD memes slider

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,12 +4,32 @@ if (burger) {
     burger.addEventListener('click', () => {
         burger.classList.toggle('opened');
         // burger.setAttribute('aria-expanded', );
-    
+
         if (burger.ariaExpanded == 'true') {
             burger.setAttribute('aria-expanded', false);
         } else {
             burger.setAttribute('aria-expanded', true);
         }
-        
+
     })
 }
+
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    slidesPerView: 1.2,
+    spaceBetween: 20,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1.6
+        },
+        900: {
+            slidesPerView: 2.9
+        }
+    }
+})
