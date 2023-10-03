64ffbc6fc4e4c22780841a2b5bfef05c6166517b
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-10-03 21:13:41 +0300

    ADD header

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,14 +1,17 @@
 const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
 
 if (burger) {
     burger.addEventListener('click', () => {
         burger.classList.toggle('opened');
-        // burger.setAttribute('aria-expanded', );
+        nav.classList.toggle("nav--open");
 
         if (burger.ariaExpanded == 'true') {
             burger.setAttribute('aria-expanded', false);
+            nav.setAttribute('aria-expanded', false);
         } else {
             burger.setAttribute('aria-expanded', true);
+            nav.setAttribute('aria-expanded', true);
         }
 
     })
